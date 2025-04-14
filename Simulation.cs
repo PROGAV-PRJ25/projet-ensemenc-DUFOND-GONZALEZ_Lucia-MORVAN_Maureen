7c9c94335495b8aaecfdbcb2d0d9cb155cdbc7c3
LuciaDG13 <ldufondgonz@ensc.fr>
2025-04-14 10:21:50 +0200

    classe simulation v1

---
+++ Simulation.cs
@@ -0,0 +1,33 @@
+public class Simulation
+{
+    public Monde monde1 { get; private set; }
+    //public List<Plante> listeRobot = new List<Plante>();
+
+    public Simulation(Monde monde1)
+    {
+        this.monde1 = monde1;
+    }
+
+    public void Simuler()
+    {
+        int i = 0;
+        while (i < 30)
+        {
+            foreach (Plante plante in listePlante)
+            {
+                /*robot.SeDeplacer();
+                robot.RealiserAction();*/
+                // Mettre les fonctions dont on aura besoin
+            }
+            // Robots parcourent le monde
+            monde1.AfficherGrille();
+            System.Threading.Thread.Sleep(3000);
+            Console.WriteLine ("\n\n");
+            // Console.Clear();
+            i++;
+
+        }
+
+    }
+
+}
